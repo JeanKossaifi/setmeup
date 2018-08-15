8a43ca577f431589728832e9786cf68c0910ce73
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2018-08-15 01:21:44 -0400

    ENH: Added snippets from daniel mai's emacs as a submodule and then symlinked them above

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -28,22 +28,3 @@
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 (setq gc-cons-threshold 800000)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-default-notes-file (concat org-directory "/notes.org"))
- '(org-directory "~/Dropbox/orgfiles")
- '(org-export-html-postamble nil)
- '(org-hide-leading-stars t)
- '(org-startup-folded 'overview)
- '(org-startup-indented t)
- '(package-selected-packages '(counsel try use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
-
